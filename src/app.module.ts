5c2e80347e1fcc4e4a0a69671e1e8f0ea225ceec
Marcelo Henrique da Silva <ra-20144422-2@alunos.unicesumar.edu.br>
2023-09-25 20:44:56 -0300

    dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { YModule } from './y/y.module';
 import { ViagemModule } from './viagem/viagem.module';
 import { DestinoModule } from './destino/destino.module';
 
 @Module({
-  imports: [YModule, ViagemModule, DestinoModule],
+  imports: [ViagemModule, DestinoModule],
   controllers: [AppController],
   providers: [AppService],
 })
